3b9392226e329b71e0e03a122164cae4f22bc6cc
Saac Sornchai <saacsos@gmail.com>
2019-03-23 15:38:52 +0700

    Main use Spring Bean

--- src/main/java/atm/Main.java
+++ src/main/java/atm/Main.java
@@ -1,11 +1,18 @@
 package atm;
 
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        DataSource dataSource = new DataSource("customers.txt");
-        Bank bank = new Bank(dataSource);
-        ATM atm = new ATM(bank);
-        ATMSimulator atmSimulator = new ATMSimulator(atm);
+//        CSVDataSource dataSource = new CSVDataSource("customers.txt");
+//        Bank bank = new Bank(dataSource);
+//        ATM atm = new ATM(bank);
+//        ATMSimulator atmSimulator = new ATMSimulator(atm);
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        ATMSimulator atmSimulator = context.getBean("atmSimulator", ATMSimulator.class);
         atmSimulator.run();
     }
 }
